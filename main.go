34fc612eab7dc79cd2ec4c01d1b54f7522919dab
Dorivika <avivek796@gmail.com>
2023-12-25 15:34:17 -0500

    third push

--- main.go
+++ main.go
@@ -1,19 +1,42 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
+	"runtime"
 	"strconv"
 
 	"github.com/Dorivika/Go-Blockchain/blockchain"
 )
 
-func main() {
-	chain := blockchain.InitBlockChain()
-	chain.AddBlock("First BLock")
-	chain.AddBlock("second BLock")
-	chain.AddBlock("third BLock")
+type CommandLine struct {
+	blockchain *blockchain.BlockChain
+}
 
-	for _, block := range chain.BLocks {
+func (cli *CommandLine) printUsage() {
+	fmt.Println(("Functionalities"))
+	fmt.Println(("add -block BLOCK_DATA - add a block to the blockchain"))
+	fmt.Println(("print - prints the blockchain"))
+}
+
+func (cli *CommandLine) validateArgs() {
+	if len(os.Args) < 2{
+		cli.printUsage()
+		runtime.Goexit()
+	}
+}
+
+func (cli *CommandLine) addBlock(data string) {
+	cli.blockchain.AddBlock(data)
+	fmt.Println("Added the Block Successfully!")
+}
+func (cli *CommandLine) printChain() {
+	iter := cli.blockchain.Iterator()	
+
+	for {
+		block:= iter.Next()
+
 		fmt.Printf("Previous Hash: %x\n", block.PrevHash)
 		fmt.Printf("Data in Block: %s\n", block.Data)
 		fmt.Printf("Hash: %x\n", block.Hash)
@@ -22,6 +45,56 @@ func main() {
 		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
 		fmt.Println()
 
+		if len(block.PrevHash) == 0 {
+			break
+		}
+
 	}
+}
+
+func (cli *CommandLine) run() {
+	cli.validateArgs()
+
+	addBlockCmd := flag.NewFlagSet("add", flag.ExitOnError)
+	printChainCmd := flag.NewFlagSet("print", flag.ExitOnError)
+	addBlockData := addBlockCmd.String("block", "", "Block data")
+
+	switch os.Args[1] {
+	case "add": 
+	err := addBlockCmd.Parse(os.Args[2:])
+	blockchain.Handle(err)
+
+	case "print":
+		err:= printChainCmd.Parse(os.Args[2:])
+		blockchain.Handle(err)
+
+	default:
+		cli.printUsage()
+		runtime.Goexit()
+
+	}
+
+	if addBlockCmd.Parsed() {
+		if *addBlockData == "" {
+			addBlockCmd.Usage()
+			runtime.Goexit()
+		}
+		cli.addBlock((*addBlockData))
+	}
+
+	if printChainCmd.Parsed() {
+		cli.printChain()
+	}
+
+	
+}
+
+func main() {
+	defer os.Exit(0)
+	chain := blockchain.InitBlockChain()
+	defer chain.Database.Close()
+
+	cli := CommandLine{chain}
+	cli.run()
 
 }
